271ead3709887a975c981794deafdddcc5634e91
Daniyal Ali <daniyaljatt275@gmail.com>
2025-11-11 20:17:58 +0500

    learning about middleware

--- express/mini-exercise-1/server.js
+++ express/mini-exercise-1/server.js
@@ -12,11 +12,24 @@ app.get("/about",(req,res)=>{
     res.send("This is about page!");
 });
  
+
 // contact page
 app.get("/contact",(req,res)=>{
     res.send("This is contact page!");
 });
 
+// services page
+app.get("/services",(req,res)=>{
+  res.send("Our Services Page!");
+});
+
+// middleware 
+app.use((req,res,next)=>{
+  console.log("Request Recieved");
+  next();
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
