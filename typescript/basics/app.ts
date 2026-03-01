452523d086fdec5f0eae3ce2de16bf48c3c99b1c
daniyalalii <daniyaljatt275@gmail.com>
2026-03-01 15:57:36 +0500

    learning about advance concepts of typescript

--- typescript/basics/app.ts
+++ typescript/basics/app.ts
@@ -1,56 +1,83 @@
-let name1: string = "Daniyal";
-let age: number = 20;
-let isAdmin: boolean = true;
-console.log(`Name: ${name1}, Age: ${age}, isAdmin: ${isAdmin}`);
+// let name1: string = "Daniyal";
+// let age: number = 20;
+// let isAdmin: boolean = true;
+// console.log(`Name: ${name1}, Age: ${age}, isAdmin: ${isAdmin}`);
 
-function add(a: number, b: number):number{
-    return a+b;
-}
+// function add(a: number, b: number):number{
+//     return a+b;
+// }
 
-// console.log(add("10",20)); compile time error 
-console.log(add(10,20));
+// // console.log(add("10",20)); compile time error 
+// console.log(add(10,20));
 
-// const user: {name: string, role: string} = {
-//     name: "Ali",
-//     role: "admin"
+// // const user: {name: string, role: string} = {
+// //     name: "Ali",
+// //     role: "admin"
+// // };
+
+// // console.log(user.name, user.role);
+
+// interface User{
+//     id: string
+//     name: string
+//     email: string
+//     role: "admin" | "user"  
+// }
+// const person1 : User = {
+//     id: "1",
+//     name: "Daniyal",
+//     email : "123@gmail.com",
+//     role: "user"
 // };
 
-// console.log(user.name, user.role);
+// interface Product {
+//     id: string
+//     name: string
+//     price: number
+//     stock?: number // ? means optional (may or may not)
+// }
 
-interface User{
-    id: string
-    name: string
-    email: string
-    role: "admin" | "user"  
+// const product1: Product = {
+//     id: "101",
+//     name : "Laptop",
+//     price : 200000,
+//     stock : 1000
+// }
+
+// console.log(product1.stock);
+
+// let numbers: number[] = [1, 2, 3]
+// console.log(numbers);
+
+// // union types for api's
+// let stat: "success" | "fail" | "error";
+// stat = "success"; // valid
+// // stat = 2; invalid
+// console.log(stat);
+
+function identity<T>(value: T): T{
+    return value;
 }
-const person1 : User = {
-    id: "1",
-    name: "Daniyal",
-    email : "123@gmail.com",
-    role: "user"
-};
 
-interface Product {
-    id: string
-    name: string
-    price: number
-    stock?: number // ? means optional (may or may not)
+console.log(identity("Name"))
+console.log(identity(4))
+
+function getLength<T extends {length : number}>(value: T){
+    return value.length
 }
 
-const product1: Product = {
-    id: "101",
-    name : "Laptop",
-    price : 200000,
-    stock : 1000
+console.log(getLength("hello"));
+console.log(getLength([1,2,3]));
+
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K]{
+    return obj[key]
 }
 
-console.log(product1.stock);
+const user = {
+    name: "Ali",
+    age: 20
+}
 
-let numbers: number[] = [1, 2, 3]
-console.log(numbers);
-
-// union types for api's
-let stat: "success" | "fail" | "error";
-stat = "success"; // valid
-// stat = 2; invalid
-console.log(stat);
+console.log(getProperty(user,"name"));
+console.log(getProperty(user,"age"));
+// console.log(getProperty(user,"id")) not valid, error
