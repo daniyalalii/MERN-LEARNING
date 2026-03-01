454d64bd5f732e30faec8da9777b4a6354ed279e
daniyalalii <daniyaljatt275@gmail.com>
2026-03-01 13:21:43 +0500

    dist folder--> for compiled production ready for easy handling for bugs and figure out what is happening , .map files easy for debugging, and .ts files to get full intellisense and type safety

---
+++ typescript/node_express/dist/app.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const app = express();
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send("Server running! 🚀 TypeScript + Express is working!");
+});
+export default app;
+//# sourceMappingURL=app.js.map
