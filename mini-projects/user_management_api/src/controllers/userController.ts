dc21f0b2837391497350d3088dc0ad0bd25022de
daniyalalii <daniyaljatt275@gmail.com>
2026-03-10 17:11:58 +0500

    created createUserController

--- mini-projects/user_management_api/src/controllers/userController.ts
+++ mini-projects/user_management_api/src/controllers/userController.ts
@@ -1,6 +1,14 @@
 import type { Request, Response } from "express";
-// import { createUser } from "../services/userService.js";
-import {getUserByIdService} from "../services/userService.js"
+import {createUser, getUserByIdService} from "../services/userService.js"
+
+export async function createUserController(req: Request, res: Response) {
+  const result = createUser(req.body)
+
+  if(result.status==="error"){
+    return res.status(400).json(result)
+  }
+  return res.status(201).json(result)
+}
 
 export async function getUserController(req: Request, res: Response) {
   const id = typeof req.params.id === "string" ? req.params.id : "";
