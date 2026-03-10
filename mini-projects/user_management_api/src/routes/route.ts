bc39b4311f00e40b6bdcba174cc8b70fbd685dd6
daniyalalii <daniyaljatt275@gmail.com>
2026-03-10 17:12:58 +0500

    added route.ts

---
+++ mini-projects/user_management_api/src/routes/route.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { validate } from "../middleware/validate.js";
+import { createUserSchema } from "../schemas/user.schema.js";
+import {
+  getUserController,
+  createUserController,
+} from "../controllers/userController.js";
+
+export const router = Router();
+
+router.post("/users", validate(createUserSchema), createUserController);
+
+router.get("/users/:id", getUserController);
