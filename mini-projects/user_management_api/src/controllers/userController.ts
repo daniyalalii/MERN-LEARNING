12eafe40ad3f79f094eb5403fa59fc9857b973ce
daniyalalii <daniyaljatt275@gmail.com>
2026-03-10 15:49:36 +0500

    updated the user controller to work as async

--- mini-projects/user_management_api/src/controllers/userController.ts
+++ mini-projects/user_management_api/src/controllers/userController.ts
@@ -1,26 +1,14 @@
 import type { Request, Response } from "express";
 // import { createUser } from "../services/userService.js";
-import {getUserById} from "../services/userService.js"
+import {getUserByIdService} from "../services/userService.js"
 
-export function getUserController(req: Request, res: Response) {
+export async function getUserController(req: Request, res: Response) {
   const id = typeof req.params.id === "string" ? req.params.id : "";
-  const result = getUserById(id);
+  const result = await getUserByIdService(id);
 
-  if (!result.ok) {
-    switch (result.error.type) {
-      case "NOT_FOUND":
-        return res.status(404).json(result.error);
-
-      case "VALIDATION_ERROR":
-        return res.status(400).json(result.error);
-
-      case "CONFLICT":
-        return res.status(409).json(result.error);
-      
-      case "AUTH_ERROR":
-        return res.status(400).json(result.error);
-    }
+  if(!result.ok){
+    return res.status(404).json(result.error);
   }
 
-  return res.json(result.value);
+  return res.json(result.value); 
 }
