b6ac07c04b670a339b7fad47c12a709930f7c5e8
daniyalalii <daniyaljatt275@gmail.com>
2026-03-14 22:42:38 +0500

    updated server file to integrate the user management api (fully)

--- mini-projects/user_management_api/src/server.ts
+++ mini-projects/user_management_api/src/server.ts
@@ -1,7 +1,22 @@
-import { app } from "./app.js";
+import "dotenv/config";
+import express from "express";
+import { connectDB } from "./config/db.js";
+import userRoutes from "./modules/users/user.routes.js"
+import { globalErrorHandler } from "./middleware/error.middleware.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000
+
+const app = express();
+
+app.use(express.json());
+
+connectDB();
+app.use(
+    "/users", userRoutes
+)
+app.use(globalErrorHandler);
+
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
